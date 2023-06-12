825cb1dbebf7c05c447730261bd315d163a4be5c
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-12 15:57:43 +0200

    Inizializzazione esercizio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+    CONSEGNA
+    1)Chiedere all'utente il numero di km che vuole percorrere e l'età del passeggero
+    2)Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
+        2a)
+
+*/
+
+
