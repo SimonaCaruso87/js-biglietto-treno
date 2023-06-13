967e7c37249fa98042f6591607d02a64a1e04ea9
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-13 15:49:45 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -11,54 +11,56 @@
     3)L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
         
 
-toFixed()
 
 */
 
-// 1)chiedere all'utente il numero di km che vuole percorrere e l'età del passeggero
+// 1)Chiedere all'utente il numero di km che vuole percorrere
 
-let userDataVoyage = prompt ('Quanti Chilometri vuoi percorrere?');
+const userDataVoyage = parseInt (prompt ('Inserisci il numero dei Km che dovrai percorrere'));
 
-let userDataAge = prompt ('Quanti anni hai?');
+console.log('userDataVoyage', userDataVoyage , typeof userDataVoyage);
 
+//2)Chiedere all'utente l'età
+const userDataAge = parseInt (prompt ('Inserisci la tua età'));
 
-//converto le stringhe in numeri
-userDataVoyage = parseInt(userDataVoyage);
-userDataAge = parseInt(userDataAge);
+console.log('userDataAge', userDataAge , typeof userDataAge);
 
-console.log( userDataVoyage , userDataAge);
+// 3)Calcolare il prezzo totale del viaggio in base alle info dell'utente
 
-document.getElementById("userdate").innerHTML=userDataAge + userDataVoyage
+//4)Verifica l'età dell'utente
+if( userDataAge < 18){
+    //applico uno sconto del 20%
+    const price = 0.21 * userDataVoyage;
+    const discount = price * 0.20;
+    const priceTotal = price - discount;
 
-//DA FARE: controllare che l'input sia un numero con isNaN
+    document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
+}    
+
+else if( userDataAge >= 65){
+    //gli applico uno sconto del 40%
+    const price = 0.21 * userDataVoyage;
+    const discount = price * 0.40;
+    const priceTotal = price - discount;
+
+    document.querySelector('h4').innerHTML = 'Il Prezzo del biglietto è: €' + priceTotal.toFixed(2);
+}
+
+else{
+    //non gli applico nessuno sconto
+    const price = 0.21 * userDataVoyage ;
+    console.log('Il Prezzo a persona è' , price);
+
+    document.querySelector('h4').innerHTML = 'Il Prezzo del Biglietto è: €' + price.toFixed(2);
+}
+
+//Inserisci un allert
 
 if(isNaN(userDataAge)){
+    //se l'utente non inserisce un numero nel campo Età
     alert('Devi inserire un numero');
 }
-
-// 2)Calcolare il prezzo totale del viaggio in base a queste info
-
-const pricePerKm = 0.21 ;
-
-const priceTotal = userDataVoyage * pricePerKm ;
-
-let discount; 
-
-if(c1){b1}
-if(c2){b2}
-
-if(c1){b1}
-else if(c2){b2}
-
-
-if( userDataAge < 18){
-    discount = priceTotal * 0.2;
-    priceTotal -= discount; // <==> priceTotal = priceTotal - discount
-}else if( userDataAge > 65){
-    discount = priceTotal * 0.4;
-    priceTotal -= discount; // <==> priceTotal = priceTotal - discount
+if(isNaN(userDataVoyage)){
+    //se l'utente non inserisce un numero nel campo Km
+    alert('Devi inserire i chilometri');
 }
-
-
-
-console.log(priceTotal)
